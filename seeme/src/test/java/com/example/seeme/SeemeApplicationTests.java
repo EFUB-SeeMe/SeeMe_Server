6db7152bc2dc1a1fcb25db18fd4b6099ef7fab3c
nitronium102 <nitronium@ewhain.net>
2021-07-02 22:27:15 +0900

    initialization

---
+++ seeme/src/test/java/com/example/seeme/SeemeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.seeme;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SeemeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
