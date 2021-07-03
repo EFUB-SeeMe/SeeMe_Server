8c89a7fb37495cf2c20017486aae21ec15bad8dd
nitronium102 <nitronium@ewhain.net>
2021-07-03 09:11:58 +0900

    initialization

--- src/main/java/com/seeme/SeemeApplication.java
+++ src/main/java/com/seeme/SeemeApplication.java
@@ -1,4 +1,4 @@
-package com.example.seeme;
+package com.seeme;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- src/test/java/com/seeme/SeemeApplicationTests.java
+++ src/test/java/com/seeme/SeemeApplicationTests.java
@@ -1,4 +1,4 @@
-package com.example.seeme;
+package com.seeme;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
