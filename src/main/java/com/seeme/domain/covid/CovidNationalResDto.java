a3cbb7b5c9b8e41c199bf540f455a84d31e5cdfa
hoit1302 <jueun000101@gmail.com>
2021-07-07 15:37:26 +0900

    feat(CovidResDto): add ResDtos about covid

---
+++ src/main/java/com/seeme/domain/covid/CovidNationalResDto.java
@@ -0,0 +1,11 @@
+package com.seeme.domain.covid;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+
+@Getter
+@Builder
+@AllArgsConstructor
+public class CovidNationalResDto {
+}
