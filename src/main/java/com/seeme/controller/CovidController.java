279b707f8bdee5e2def9dd4e76cf36875eff52c8
hoit1302 <jueun000101@gmail.com>
2021-07-07 15:37:58 +0900

    feat(CovidController): add GET /covid

---
+++ src/main/java/com/seeme/controller/CovidController.java
@@ -0,0 +1,30 @@
+package com.seeme.controller;
+
+import com.seeme.service.CovidService;
+import lombok.AllArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@AllArgsConstructor
+@RequestMapping("covid")
+public class CovidController {
+	public final CovidService covidService;
+
+	@GetMapping("/main")
+	public ResponseEntity<Object> getMain() {
+		return ResponseEntity.ok().body(covidService.getMain());
+	}
+
+	@GetMapping("/national")
+	public ResponseEntity<Object> getNational() {
+		return ResponseEntity.ok().body("전국");
+	}
+
+	@GetMapping("/regional")
+	public ResponseEntity<Object> getRegional() {
+		return ResponseEntity.ok().body("지역");
+	}
+}
