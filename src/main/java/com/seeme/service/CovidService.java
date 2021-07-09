f9c6a0bb0b155ade3764c153819865c009405b64
Sukyeong Seo <80563849+sukyeongs@users.noreply.github.com>
2021-07-09 11:33:39 +0900

    feat(CovidService): add some covid-national logic

--- src/main/java/com/seeme/service/CovidService.java
+++ src/main/java/com/seeme/service/CovidService.java
@@ -1,4 +1,29 @@
 package com.seeme.service;
 
+import com.seeme.domain.CovidNationalResDto;
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.xml.sax.SAXException;
+
+import javax.xml.parsers.ParserConfigurationException;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+
+@Service
+@AllArgsConstructor
 public class CovidService {
+
+    public CovidNationalResDto getMain() throws ParserConfigurationException, SAXException, IOException {
+        String location = getMainLocation();
+
+        return CovidNationalResDto.builder()
+                .location(location)
+    }
+
+    private String getMainLocation() {
+        return "남양주";
+    }
+
+    private final SimpleDateFormat DATE_FORMAT = new SimpleDateFormat("yyyyMMdd");
+    private final SimpleDateFormat TIME_FORMAT = new SimpleDateFormat("HH");
 }
