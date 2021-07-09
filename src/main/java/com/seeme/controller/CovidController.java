dc7fc82589e2eefd783379b452d5085f9055dfbf
Sukyeong Seo <80563849+sukyeongs@users.noreply.github.com>
2021-07-09 11:39:09 +0900

    feat(CovidController): add Mapping

--- src/main/java/com/seeme/controller/CovidController.java
+++ src/main/java/com/seeme/controller/CovidController.java
@@ -1,4 +1,35 @@
 package com.seeme.controller;
 
+import com.seeme.service.CovidService;
+import lombok.AllArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@AllArgsConstructor
+@RequestMapping("covid")
 public class CovidController {
+    public final CovidService covidService;
+
+    @GetMapping("/main")
+    public ResponseEntity<Object> getMain() {
+        try {
+            return ResponseEntity.ok().body(covidService.getMain());
+        } catch (Exception e) {
+            e.printStackTrace();
+            return ResponseEntity.internalServerError().body("에러");
+        }
+    }
+
+    @GetMapping("/national")
+    public ResponseEntity<Object> getNational() {
+        return ResponseEntity.ok().body("전국");
+    }
+
+    @GetMapping("/regional")
+    public ResponseEntity<Object> getRegional() {
+        return ResponseEntity.ok().body("지역");
+    }
 }
