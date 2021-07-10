0ca5676ff99312979d04ef7d3818c0d9e5da6652
nitronium102 <nitronium@ewhain.net>
2021-07-11 00:41:52 +0900

    feat(CovidRegionalResDto): add CoronicList

--- src/main/java/com/seeme/domain/covid/CovidRegionalResDto.java
+++ src/main/java/com/seeme/domain/covid/CovidRegionalResDto.java
@@ -2,10 +2,23 @@ package com.seeme.domain.covid;
 
 import lombok.AllArgsConstructor;
 import lombok.Builder;
+import lombok.Data;
 import lombok.Getter;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Getter
 @Builder
 @AllArgsConstructor
 public class CovidRegionalResDto {
+    private final Integer newCoronic;
+    private final Integer totalCoronic;
+    private final List<CoronicList> coronicList;
+}
+
+@Data
+public class CoronicList {
+    private final String day;
+    private final Integer coronic;
 }
