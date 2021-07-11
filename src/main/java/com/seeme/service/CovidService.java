a1d19cda80fbebef46129d98f627585a9f4a48de
nitronium102 <nitronium@ewhain.net>
2021-07-11 09:45:58 +0900

    feat(CovidService): add data processing logic

--- src/main/java/com/seeme/service/CovidService.java
+++ src/main/java/com/seeme/service/CovidService.java
@@ -2,8 +2,7 @@ package com.seeme.service;
 
 import com.seeme.api.CovidOpenApi;
 import com.seeme.api.LocationApi;
-import com.seeme.domain.covid.CovidDto;
-import com.seeme.domain.covid.CovidResDto;
+import com.seeme.domain.covid.*;
 import com.seeme.util.LocationUtil;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -11,6 +10,7 @@ import org.xml.sax.SAXException;
 
 import javax.xml.parsers.ParserConfigurationException;
 import java.io.IOException;
+import java.util.List;
 
 @Service
 @AllArgsConstructor
@@ -60,4 +60,31 @@ public class CovidService {
 	public String getLocation(Double longitude, Double latitude) throws Exception {
 		return LocationUtil.getLocation(locationApi.covertGpsToAddress(longitude, latitude));
 	}
+
+	public CovidRegionalResDto getRegional(String location) throws IOException, ParserConfigurationException, SAXException {
+		// list는 index를 integer로 받아서 형식이 안 맞음
+		// Hashmap 등 다른 방법 고려해야 함
+		List<CoronicList> coronicList = null;
+
+		int newCoronic = 0, totalCoronic = 0, coronic = 0;
+		String day="";
+
+		for (CovidRegionalDto RegionCovid : covidOpenApi.getRegionalApi()) {
+			if (RegionCovid.getGubun().equals(location)) {
+				day = RegionCovid.getStdDay();
+				coronic = Integer.parseInt(RegionCovid.getLocalOccCnt());
+				coronicList.add(day, coronic);
+
+				newCoronic = Integer.parseInt(RegionCovid.getLocalOccCnt());
+				totalCoronic = Integer.parseInt(RegionCovid.getDefCnt());
+			}
+		}
+
+		return CovidRegionalResDto.builder()
+			.newCoronic(newCoronic)
+			.totalCoronic(totalCoronic)
+			.CoronicList(coronicList)
+			.build();
+	}
+
 }
