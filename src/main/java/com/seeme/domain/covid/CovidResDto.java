ac8478be4d61b59de98f34e5cc8f74d9bfde467e
hoit1302 <jueun000101@gmail.com>
2021-07-12 10:22:21 +0900

    refactor(CovidResDto): remove unnecessary annoatation

--- src/main/java/com/seeme/domain/covid/CovidResDto.java
+++ src/main/java/com/seeme/domain/covid/CovidResDto.java
@@ -1,12 +1,10 @@
 package com.seeme.domain.covid;
 
-import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 
 @Getter
 @Builder
-@AllArgsConstructor
 public class CovidResDto {
 	private final String location;
 	private final Integer coronicTotal;
