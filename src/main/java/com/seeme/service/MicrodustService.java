81c33a59811efa2558fa1f2aa62b258dbafbb4c2
Sukyeong Seo <80563849+sukyeongs@users.noreply.github.com>
2021-07-15 21:49:01 +0900

    feat(MicrodustService): add getTimeApi() logic

--- src/main/java/com/seeme/service/MicrodustService.java
+++ src/main/java/com/seeme/service/MicrodustService.java
@@ -3,11 +3,20 @@ package com.seeme.service;
 import com.seeme.api.MicrodustOpenApi;
 import com.seeme.domain.microdust.Microdust;
 import com.seeme.domain.microdust.MicrodustResDto;
+import com.seeme.domain.microdust.MicrodustTimeDto;
+import com.seeme.domain.microdust.MicrodustTimeResDto;
 import com.seeme.util.MicrodustUtil;
 import lombok.AllArgsConstructor;
 import org.json.simple.parser.ParseException;
 import org.springframework.stereotype.Service;
+import org.xml.sax.SAXException;
+
+import javax.xml.parsers.ParserConfigurationException;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+import static com.seeme.util.MicrodustUtil.getDataTime;
 
 @Service
 @AllArgsConstructor
@@ -26,6 +35,28 @@ public class MicrodustService {
 			.gradeIcon(MicrodustUtil.getGradeIcon(microdust.getPmGrade()))
 			.desc(MicrodustUtil.getDesc(microdust.getPmGrade()))
 			.build();
+	}
+
+	public MicrodustTimeResDto getTime(String measuringStation) throws IOException, ParseException, ParserConfigurationException, SAXException {
+		List<MicrodustTimeResDto> microdustTimeResDtoList = new ArrayList<>();
+		int pm10 = 12, pm25 = 10;
+
+		for (MicrodustTimeDto microdustTimeDto : microdustOpenApi.getTimeApi(measuringStation)) {
+			if (microdustTimeDto.getStationName().equals(measuringStation)) {
+				microdustTimeResDtoList.add(MicrodustTimeResDto.builder()
+						.time(microdustTimeDto.getTime())
+						.pm10(Integer.parseInt(microdustTimeDto.getPm10Value()))
+						.pm25(Integer.parseInt(microdustTimeDto.getPm25Value()))
+						.build());
+			}
+		}
+
+		return MicrodustTimeResDto.builder()
+				.time(getDataTime())
+				.pm10(pm10)
+				.pm25(pm25)
+				.build();
+
 	}
 
 	public String getMeasuringStation(Double lat, Double lon) throws IOException, ParseException {
