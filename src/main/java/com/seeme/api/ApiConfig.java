23963d10c170b52c57dae400e03cb3784a9a1bd4
hoit1302 <jueun000101@gmail.com>
2021-07-15 21:41:17 +0900

    feat(ApiConfig): add api url

--- src/main/java/com/seeme/api/ApiConfig.java
+++ src/main/java/com/seeme/api/ApiConfig.java
@@ -14,8 +14,10 @@ public class ApiConfig {
 	private String covidMainUrl;
 	private String locationUrl;
 	private String locationKey;
+	private String convertWGS84ToTMAuthUrl;
+	private String convertWGS84ToTMAuthId;
+	private String convertWGS84ToTMAuthKey;
 	private String convertWGS84ToTMUrl;
-	private String convertWGS84ToTMKey;
 	private String microdustStationUrl;
 	private String microdustMainUrl;
 	private String microdustMainKey;
