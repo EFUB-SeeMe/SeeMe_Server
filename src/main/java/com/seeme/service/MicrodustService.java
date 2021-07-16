b501da5de70dc1336f6c0bfd6348238e8ca94597
hoit1302 <jueun000101@gmail.com>
2021-07-16 17:04:11 +0900

    feat(MicrodustService): add search logic to /microdust/main

--- src/main/java/com/seeme/service/MicrodustService.java
+++ src/main/java/com/seeme/service/MicrodustService.java
@@ -1,7 +1,8 @@
 package com.seeme.service;
 
-import com.seeme.api.LocationApi;
-import com.seeme.api.MicrodustOpenApi;
+import com.seeme.service.api.LocationApi;
+import com.seeme.service.api.MicrodustOpenApi;
+import com.seeme.domain.location.TMAddress;
 import com.seeme.domain.microdust.Microdust;
 import com.seeme.domain.microdust.MicrodustResDto;
 import com.seeme.domain.microdust.MicrodustTimeDto;
@@ -17,8 +18,6 @@ import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
-import static com.seeme.util.MicrodustUtil.getDataTime;
-
 @Service
 @AllArgsConstructor
 public class MicrodustService {
@@ -26,8 +25,8 @@ public class MicrodustService {
 	private final LocationApi locationApi;
 	private final MicrodustOpenApi microdustOpenApi;
 
-	public MicrodustResDto getMain(String measuringStation, String address) throws IOException, ParseException {
-		Microdust microdust = microdustOpenApi.getMainApi(measuringStation);
+	public MicrodustResDto getMain(List<String> measuringStationList, String address) throws IOException, ParseException {
+		Microdust microdust = microdustOpenApi.getMainApi(measuringStationList);
 		System.out.println(microdust.toString()); // after dev, remove
 		return MicrodustResDto.builder()
 			.address(address)
@@ -39,12 +38,15 @@ public class MicrodustService {
 			.build();
 	}
 
-	public List<MicrodustTimeResDto> getTime(String measuringStation) throws IOException, ParseException, ParserConfigurationException, SAXException {
-		List<MicrodustTimeResDto> microdustTimeResDtoList = new ArrayList<>();
-		int pm10 = 12, pm25 = 10;
+	public TMAddress getTMAddress(String location) throws IOException {
+		return locationApi.getTMAddress(location);
+	}
 
-		for (MicrodustTimeDto microdustTimeDto : microdustOpenApi.getTimeApi(measuringStation)) {
-			if (microdustTimeDto.getStationName().equals(measuringStation)) {
+	public List<MicrodustTimeResDto> getTime(List<String> stationList) throws IOException, ParserConfigurationException, SAXException {
+		List<MicrodustTimeResDto> microdustTimeResDtoList = new ArrayList<>();
+
+		for (MicrodustTimeDto microdustTimeDto : microdustOpenApi.getTimeApi(stationList)) {
+			if (microdustTimeDto.getStationName().equals(stationList)) {
 				microdustTimeResDtoList.add(MicrodustTimeResDto.builder()
 						.time(microdustTimeDto.getTime())
 						.pm10(Integer.parseInt(microdustTimeDto.getPm10Value()))
@@ -56,12 +58,20 @@ public class MicrodustService {
 		return microdustTimeResDtoList;
 	}
 
-	public String getMeasuringStation(Double lat, Double lon) throws IOException, ParseException {
-		return microdustOpenApi.getMeasuringStation(lat, lon);
+	public List<String> getStationList(Double lat, Double lon) throws IOException, ParseException {
+		return microdustOpenApi.getStationList(lat, lon);
 	}
 
 	public String getAddress(Double lat, Double lon) throws Exception {
 		return locationApi.covertGpsToSpecificAddress(lat, lon);
+	}
+
+	public String getAddressByTM(TMAddress tmAddress) {
+		return tmAddress.getSggName() + " " + tmAddress.getUmdName();
+	}
+
+	public List<String> getStationListByTM(String tmX, String tmY) throws IOException, ParseException {
+		return microdustOpenApi.getStationListByTM(tmX, tmY);
 	}
 }
 
