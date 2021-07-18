29766d0177f0e9b73a31bb7641d73967e7db96fa
Sukyeong Seo <80563849+sukyeongs@users.noreply.github.com>
2021-07-19 04:42:46 +0900

    feat(MicrodustController): change getTime() logic

--- src/main/java/com/seeme/controller/MicrodustController.java
+++ src/main/java/com/seeme/controller/MicrodustController.java
@@ -1,6 +1,7 @@
 package com.seeme.controller;
 
 import com.seeme.domain.location.TMAddress;
+import com.seeme.domain.microdust.MicrodustTimeResDto;
 import com.seeme.service.MicrodustService;
 import lombok.AllArgsConstructor;
 import org.springframework.http.ResponseEntity;
@@ -10,6 +11,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.Arrays;
+import java.util.List;
 
 @Controller
 @AllArgsConstructor
@@ -20,20 +22,20 @@ public class MicrodustController {
 
 	@GetMapping("/main")
 	public ResponseEntity<Object> getMain(@RequestParam(required = false) String location,
-		@RequestParam(required = false) Double lat, @RequestParam(required = false) Double lon) {
+										  @RequestParam(required = false) Double lat, @RequestParam(required = false) Double lon) {
 		try {
 			if (location != null) {
 				TMAddress tmAddress = microdustService.getTMAddress(location);
 				return ResponseEntity.ok().body(microdustService.getMain(
-					microdustService.getStationListByTM(tmAddress.getTmX(), tmAddress.getTmY()),
-					microdustService.getAddressByTM(tmAddress)));
+						microdustService.getStationListByTM(tmAddress.getTmX(), tmAddress.getTmY()),
+						microdustService.getAddressByTM(tmAddress)));
 			} else if (lat != null && lon != null)
 				return ResponseEntity.ok().body(microdustService.getMain(
-					microdustService.getStationList(lat, lon),
-					microdustService.getAddress(lat, lon)));
+						microdustService.getStationList(lat, lon),
+						microdustService.getAddress(lat, lon)));
 			else
 				return ResponseEntity.ok().body(microdustService.getMain(
-					Arrays.asList("중구", "한강대로", "청계천로"), "서울특별시 중구 서소문동"));
+						Arrays.asList("중구", "한강대로", "청계천로"), "서울특별시 중구 서소문동"));
 		} catch (Exception e) {
 			e.printStackTrace();
 			return ResponseEntity.internalServerError().body("internal server error");
@@ -42,18 +44,25 @@ public class MicrodustController {
 
 	@GetMapping("/time")
 	public ResponseEntity<Object> getTime(
-		@RequestParam(required = false) Double lat, @RequestParam(required = false) Double lon) {
+			@RequestParam(required = false)  Double lat, Double lon) {
+		String location = lat+","+lon;
 		try {
-			if (lat == null || lon == null)
-				return ResponseEntity.ok().body(microdustService.getTime("중구"));
-//					Arrays.asList("중구", "한강대로", "청계천로")));
-			else
-				return ResponseEntity.ok().body(microdustService.getTime("중구"));
-//					microdustService.getStationList(lat, lon)));
+			if (lat == null || lon == null) {
+				MicrodustTimeResDto microdustTimeResDto = microdustService.getFirstTime(microdustService.getStationList(37.56197784552834, 126.9468124393769));
+				List<MicrodustTimeResDto> microdustTimeResDtoList = microdustService.getOtherTime("37.56197784552834,126.9468124393769");
+				microdustTimeResDtoList.add(0, microdustTimeResDto);
+				return ResponseEntity.ok().body(microdustService.getOtherTime("37.56197784552834,126.9468124393769"));
+			}
+			else {
+				MicrodustTimeResDto microdustTimeResDto = microdustService.getFirstTime(microdustService.getStationList(lat, lon));
+				microdustService.getOtherTime(location).add(0, microdustTimeResDto);
+				return ResponseEntity.ok().body(microdustService.getOtherTime(location));
+			}
 		} catch (Exception e) {
 			e.printStackTrace();
 			return ResponseEntity.internalServerError().body("internal server error");
 		}
+
 	}
 
 	@GetMapping("/day")
