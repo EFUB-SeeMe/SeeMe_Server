089483f2ba502475dc078030de567ce7c03720a1
hoit1302 <jueun000101@gmail.com>
2021-07-19 10:49:03 +0900

    feat(ApiConfig): add microdust map url

--- src/main/java/com/seeme/service/api/ApiConfig.java
+++ src/main/java/com/seeme/service/api/ApiConfig.java
@@ -27,4 +27,6 @@ public class ApiConfig {
 	private String TMAddressKey;
 	private String microdustDayUrl;
 	private String microdustDayKey;
+	private String microdustMapUrl;
+	private String microdustMapKey;
 }
