8d4493044062bb1274addbd34db03e39d13b59ce
hoit1302 <jueun000101@gmail.com>
2021-07-20 01:23:38 +0900

    feat(CORSConfig): add CORS configuration

---
+++ src/main/java/com/seeme/common/CORSConfig.java
@@ -0,0 +1,16 @@
+package com.seeme.common;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class CORSConfig implements WebMvcConfigurer {
+	@Override
+	public void addCorsMappings(CorsRegistry registry) {
+		registry.addMapping("/**")
+			.allowedOrigins("*", "http://localhost:3000")
+			.allowedMethods("GET")
+			.maxAge(3000);
+	}
+}
