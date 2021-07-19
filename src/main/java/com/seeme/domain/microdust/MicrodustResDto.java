17e76b5e53cff97d378c7001058a39049906f497
nitronium102 <nitronium@ewhain.net>
2021-07-19 18:15:30 +0900

    style(MicrodustDtos): change int to Integer

--- src/main/java/com/seeme/domain/microdust/MicrodustResDto.java
+++ src/main/java/com/seeme/domain/microdust/MicrodustResDto.java
@@ -9,7 +9,7 @@ public class MicrodustResDto {
 	private final String address;
 	private final String grade;
 	private final String gradeIcon;
-	private final int pm10;
-	private final int pm25;
+	private final Integer pm10;
+	private final Integer pm25;
 	private final String desc;
 }
