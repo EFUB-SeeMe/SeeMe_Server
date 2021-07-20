1c32a737416f80e0ff326ee286cfef03717b4d0c
hoit1302 <jueun000101@gmail.com>
2021-07-20 12:10:06 +0900

    feat(LocationController): add GET /location/search

--- src/main/java/com/seeme/controller/LocationController.java
+++ src/main/java/com/seeme/controller/LocationController.java
@@ -2,6 +2,7 @@ package com.seeme.controller;
 
 import com.seeme.service.LocationService;
 import lombok.AllArgsConstructor;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -21,6 +22,16 @@ public class LocationController {
 		} catch (Exception e) {
 			e.printStackTrace();
 			return "지원하지 않는 위치입니다.";
+		}
+	}
+
+	@GetMapping("/search")
+	public ResponseEntity<Object> searchByUmd (@RequestParam String umd) {
+		try {
+			return ResponseEntity.ok().body(locationService.searchByUmd(umd));
+		} catch (Exception e) {
+			e.printStackTrace();
+			return ResponseEntity.internalServerError().body("internal server error");
 		}
 	}
 }
