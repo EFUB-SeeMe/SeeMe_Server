34fefdf5808f9635da2d563ba6ebc9f0577c6a3b
Minji Kang <nitronium@ewhain.net>
2021-07-23 18:30:00 +0900

    style(MicrodustOtherResDto): indentation interval cleanup

--- src/main/java/com/seeme/domain/microdust/MicrodustOtherResDto.java
+++ src/main/java/com/seeme/domain/microdust/MicrodustOtherResDto.java
@@ -6,19 +6,19 @@ import lombok.Builder;
 @Getter
 @Builder
 public class MicrodustOtherResDto {
-	private final Boolean pm10Flag;
-	private final Boolean pm25Flag;
-	private final Boolean so2Flag;
-	private final Boolean coFlag;
-	private final Boolean o3Flag;
-	private final Boolean no2Flag;
-	private final Boolean caiFlag;
-	private final Double pm10;
-	private final Double pm25;
-	private final Double so2;
-	private final Double co;
-	private final Double o3;
-	private final Double no2;
-	private final Double cai;
-	private final String caiIcon;
+    private final Boolean pm10Flag;
+    private final Boolean pm25Flag;
+    private final Boolean so2Flag;
+    private final Boolean coFlag;
+    private final Boolean o3Flag;
+    private final Boolean no2Flag;
+    private final Boolean caiFlag;
+    private final Double pm10;
+    private final Double pm25;
+    private final Double so2;
+    private final Double co;
+    private final Double o3;
+    private final Double no2;
+    private final Double cai;
+    private final String caiIcon;
 }
