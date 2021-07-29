6a37a1ed067435fc059ea674d2ce8e572f38bc57
hoit1302 <jueun000101@gmail.com>
2021-07-29 12:23:36 +0900

    feat(MicrodustController): remove try-catch

--- src/main/java/com/seeme/controller/MicrodustController.java
+++ src/main/java/com/seeme/controller/MicrodustController.java
@@ -23,21 +23,16 @@ public class MicrodustController {
     public ResponseEntity<Object> getMain(
         @RequestParam(required = false) String code,
         @RequestParam(required = false) Double lat, @RequestParam(required = false) Double lon) {
-        try {
-            if (code != null) {
-                Address address = locationService.getAddressByCode(code);
-                return ResponseEntity.ok().body(microdustService.getMain(
-                    microdustService.getStationList(address.getLat(), address.getLon())));
-            } else if (lat != null && lon != null)
-                return ResponseEntity.ok().body(microdustService.getMain(
-                    microdustService.getStationList(lat, lon)));
-            else
-                return ResponseEntity.ok().body(microdustService.getMain(
-                    MicrodustUtil.DEFAULT_STATION));
-        } catch (Exception e) {
-            e.printStackTrace();
-            return ResponseEntity.internalServerError().body("internal server error");
-        }
+        if (code != null) {
+            Address address = locationService.getAddressByCode(code);
+            return ResponseEntity.ok().body(microdustService.getMain(
+                microdustService.getStationList(address.getLat(), address.getLon())));
+        } else if (lat != null && lon != null)
+            return ResponseEntity.ok().body(microdustService.getMain(
+                microdustService.getStationList(lat, lon)));
+        else
+            return ResponseEntity.ok().body(microdustService.getMain(
+                MicrodustUtil.DEFAULT_STATION));
     }
 
     @GetMapping("/day")
@@ -58,37 +53,4 @@ public class MicrodustController {
             return ResponseEntity.internalServerError().body("internal server error");
         }
     }
-
-	/*
-	@GetMapping("/time")
-	public ResponseEntity<Object> getTime(
-		@RequestParam(required = false) Double lat, Double lon) {
-		String location = lat + "," + lon;
-		try {
-			if (lat == null || lon == null) {
-				MicrodustTimeResDto microdustTimeResDto = microdustService.getFirstTime(microdustService.getStationList(37.56197784552834, 126.9468124393769));
-				List<MicrodustTimeResDto> microdustTimeResDtoList = microdustService.getOtherTime("37.56197784552834,126.9468124393769");
-				microdustTimeResDtoList.add(0, microdustTimeResDto);
-				return ResponseEntity.ok().body(microdustService.getOtherTime("37.56197784552834,126.9468124393769"));
-			} else {
-				MicrodustTimeResDto microdustTimeResDto = microdustService.getFirstTime(microdustService.getStationList(lat, lon));
-				microdustService.getOtherTime(location).add(0, microdustTimeResDto);
-				return ResponseEntity.ok().body(microdustService.getOtherTime(location));
-			}
-		} catch (Exception e) {
-			e.printStackTrace();
-			return ResponseEntity.internalServerError().body("internal server error");
-		}
-	}
-
-	@GetMapping("/map")
-	public ResponseEntity<Object> getMap() {
-		try {
-			return ResponseEntity.ok().body(microdustService.getMap());
-		} catch (Exception e) {
-			e.printStackTrace();
-			return ResponseEntity.internalServerError().body("internal server error");
-		}
-	}
-	 */
 }
