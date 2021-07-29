3b361e6ea4a7ca8bb9f212e9f8d95574a0d4f334
hoit1302 <jueun000101@gmail.com>
2021-07-29 13:09:19 +0900

    feat(WeatherUtil):add weather icon handling

--- src/main/java/com/seeme/util/WeatherUtil.java
+++ src/main/java/com/seeme/util/WeatherUtil.java
@@ -1,8 +1,5 @@
 package com.seeme.util;
 
-import org.json.simple.JSONObject;
-import org.springframework.format.datetime.standard.DateTimeFormatterRegistrar;
-
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.time.format.DateTimeFormatter;
@@ -22,9 +19,52 @@ public class WeatherUtil {
 	public static final String RAIN_ICON_PREFIX =
 		"https://seeme-icon.s3.ap-northeast-2.amazonaws.com/icon/weather/rain/";
 
-	public static String getIcon(Integer iconNumber){
-		// TODO : 날씨 아이콘 매칭칭
-		return  "https://seeme-icon.s3.ap-northeast-2.amazonaws.com/icon/weather/Sun.png";
+	public static String getIcon(Integer iconNumber) {
+		String icon;
+		switch (iconNumber) {
+			case 1: case 2: case 3:
+				icon = "Sun";
+				break;
+			case 15: case 18: case 26: case 29: case 41: case 42:
+				icon = "Rain-1";
+				break;
+			case 12: case 39: case 40:
+				icon = "Rain-2";
+				break;
+			case 7: case 11: case 38:
+				icon = "Cloud";
+				break;
+			case 22: case 24: case 44:
+				icon = "Snow";
+				break;
+			case 4: case 5: case 6:
+				icon = "Cloudy-Sun";
+				break;
+			case 16: case 17:
+				icon = "Sun-Rain-1";
+				break;
+			case 13: case 14:
+				icon = "Sun-Rain-2";
+				break;
+			case 19: case 20: case 21: case 23: case 25: case 43:
+				icon = "Snowy";
+				break;
+			case 30:
+				icon = "Hot";
+				break;
+			case 31:
+				icon = "Cold";
+				break;
+			case 32:
+				icon = "Wind";
+				break;
+			case 33: case 34: case 35: case 36: case 37:
+				icon = "Moon";
+				break;
+			default: // TODO: add default error icon
+				icon = "Cloud";
+		}
+		return WEATHER_ICON_PREFIX + icon + ".png";
 	}
 
 	public static String getIconDesc(Integer iconNumber){
