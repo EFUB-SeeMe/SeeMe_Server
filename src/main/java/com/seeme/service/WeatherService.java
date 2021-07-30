dbacd1dac541409521a2ffbcd88c6faeeabc9249
hoit1302 <jueun000101@gmail.com>
2021-07-30 20:59:13 +0900

    feat(WeatherService): add ootd logic

--- src/main/java/com/seeme/service/WeatherService.java
+++ src/main/java/com/seeme/service/WeatherService.java
@@ -4,9 +4,10 @@ import com.seeme.domain.ResDto;
 import com.seeme.domain.weather.*;
 import com.seeme.service.api.WeatherOpenApi;
 import com.seeme.util.ErrorMessage;
+import com.seeme.util.WeatherUtil;
 import lombok.AllArgsConstructor;
-import org.json.simple.parser.ParseException;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.io.IOException;
 import java.util.ArrayList;
@@ -17,21 +18,27 @@ import java.util.List;
 public class WeatherService {
 
 	private final WeatherOpenApi weatherOpenApi;
+	private final ClothesRepository clothesRepository;
 
 	public WeatherMainResDto getMain(Double lat, Double lon) {
 
 		try {
 			String location = weatherOpenApi.getLocationApi(lat, lon);
 			List<Weather> weather = weatherOpenApi.getForecastApi(location);
-			ResDto currents = getMainCurrent(location);
-			ResDto minmax = getMainMinMax(weather);
-			ResDto week = getWeek(weather);
+			WeatherMain current = weatherOpenApi.getMainApi(location);
+
+			ResDto currentResDto = getCurrentResDto(current);
+			ResDto minmaxResDto = getMainMinMaxResDto(weather);
+			ResDto ootdResDto = getOotdResDto(currentResDto, minmaxResDto);
+			ResDto weekResDto = getWeekResDto(weather);
+
 			return WeatherMainResDto.builder()
-				.currentInfo(currents)
-				.minmaxInfo(minmax)
-				.weekInfo(week)
+				.currentInfo(currentResDto)
+				.minmaxInfo(minmaxResDto)
+				.weekInfo(weekResDto)
+				.ootdInfo(ootdResDto)
 				.build();
-		} catch (IOException | ParseException e) {
+		} catch (IOException e) {
 			e.printStackTrace();
 			ResDto resDto = ResDto.builder()
 				.resultCode(500)
@@ -52,7 +59,7 @@ public class WeatherService {
 		}
 	}
 
-	private ResDto getMainMinMax(List<Weather> weatherList) {
+	private ResDto getMainMinMaxResDto(List<Weather> weatherList) {
 		Weather weather = weatherList.get(0);
 
 		return ResDto.builder()
@@ -66,15 +73,15 @@ public class WeatherService {
 			.build();
 	}
 
-	private ResDto getMainCurrent(String location) throws IOException, ParseException {
+	private ResDto getCurrentResDto(WeatherMain current) {
 		return ResDto.builder()
 			.resultCode(200)
 			.errorMessage(ErrorMessage.SUCCESS)
-			.document(weatherOpenApi.getMainApi(location))
+			.document(current)
 			.build();
 	}
 
-	public ResDto getWeek(List<Weather> weatherList) {
+	public ResDto getWeekResDto(List<Weather> weatherList) {
 
 		List<WeatherWeekResDto> week = new ArrayList<>();
 		for (Weather weather : weatherList) {
@@ -99,15 +106,13 @@ public class WeatherService {
 			.build();
 	}
 
-	public WeatherTimeResDto getTime(Double lat, Double lon) throws IOException {
+	public WeatherTimeResDto getTime(Double lat, Double lon) {
 		try {
 			List<WeatherTime> weatherTimeList = weatherOpenApi.getTimeApi(
 				weatherOpenApi.getLocationApi(lat, lon));
 			return WeatherTimeResDto.builder()
 				.tempInfo(getTempResDto(weatherTimeList))
 				.rainInfo(getRainResDto(weatherTimeList))
-				.ootdInfo(getOotdResDto(
-					getTempResDto(weatherTimeList), getRainResDto(weatherTimeList)))
 				.build();
 		} catch (IOException e) {
 			e.printStackTrace();
@@ -118,7 +123,7 @@ public class WeatherService {
 				.build();
 
 			return WeatherTimeResDto.builder()
-				.tempInfo(resDto).rainInfo(resDto).ootdInfo(resDto).build();
+				.tempInfo(resDto).rainInfo(resDto).build();
 		} catch (Exception e) {
 			e.printStackTrace();
 			ResDto resDto = ResDto.builder()
@@ -128,7 +133,7 @@ public class WeatherService {
 				.build();
 
 			return WeatherTimeResDto.builder()
-				.tempInfo(resDto).rainInfo(resDto).ootdInfo(resDto).build();
+				.tempInfo(resDto).rainInfo(resDto).build();
 		}
 	}
 
@@ -170,8 +175,8 @@ public class WeatherService {
 						.time(weatherTime.getTime())
 						.rain((int) Double.parseDouble(weatherTime.getRain()))
 						.percent(Integer.parseInt(weatherTime.getPercent()))
-					.icon(weatherTime.getRainIcon())
-					.build());
+						.icon(weatherTime.getRainIcon())
+						.build());
 		}
 
 		return ResDto.builder()
@@ -181,31 +186,61 @@ public class WeatherService {
 			.build();
 	}
 
-	private ResDto getOotdResDto(ResDto temp, ResDto main) {
-		// TODO: add clothes recommendation logic to here page
-		Clothes clothes = Clothes.builder()
-			.item1("https://~.png")
-			.item1Desc("하와이안 셔츠")
-			.item2("https://~.png")
-			.item2Desc("반바지")
-			.item3("https://~.png")
-			.item3Desc("선크림")
-			.item4("https://~.png")
-			.item4Desc("샌들")
-			.build();
+	private ResDto getOotdResDto(ResDto currentResDto, ResDto mixmaxResDto) {
+		// TODO: link data to temp and rainFlag variable;
+		int temp = 33;
+		Boolean rainFlag = false;
 
 		WeatherOotdResDto ootd = WeatherOotdResDto.builder()
-			.age10(clothes)
-			.age20(clothes)
-			.age30(clothes)
-			.age40(clothes)
-			.age50(clothes)
+			.age10(getClothesResDto(temp, 10, false)) // update 3rd parameter to "rainFlag";
+			.age20(getClothesResDto(temp, 20, false))
+			.age30(getClothesResDto(temp, 30, false))
+			.age40(getClothesResDto(temp, 40, true))
+			.age50(getClothesResDto(temp, 50, true))
 			.build();
 
 		return ResDto.builder()
 			.resultCode(200)
 			.errorMessage(ErrorMessage.SUCCESS)
 			.document(ootd)
+			.build();
+	}
+
+	@Transactional
+	public ClothesResDto getClothesResDto(int temp, int age, boolean rainFlag) {
+		int iconNum1, iconNum2, iconNum3, iconNum4, index;
+		String item1, item2, item3, item4;
+
+		List<Clothes> topList = clothesRepository.findAllByTempAndAgeAndCategory(temp, age, "top");
+		index = (int) (Math.random() * topList.size());
+		iconNum1 = topList.get(index).getIconNum();
+		item1 = topList.get(index).getDescription();
+
+		List<Clothes> bottomList = clothesRepository.findAllByTempAndAgeAndCategory(temp, age, "bottom");
+		index = (int) (Math.random() * bottomList.size());
+		iconNum2 = bottomList.get(index).getIconNum();
+		item2 = bottomList.get(index).getDescription();
+
+		List<Clothes> shoesList = clothesRepository.findAllByTempAndAgeAndCategory(temp, age, "shoes");
+		index = (int) (Math.random() * shoesList.size());
+		iconNum3 = shoesList.get(index).getIconNum();
+		item3 = shoesList.get(index).getDescription();
+
+		if (rainFlag) {
+			iconNum4 = 28;
+			item4 = "우산 챙기세요!";
+		} else {
+			List<Clothes> itemList = clothesRepository.findAllByTempAndAgeAndCategory(temp, age, "item");
+			index = (int) (Math.random() * itemList.size());
+			iconNum4 = itemList.get(index).getIconNum();
+			item4 = itemList.get(index).getDescription();
+		}
+
+		return ClothesResDto.builder()
+			.item1(WeatherUtil.getClothesIcon(iconNum1)).item1Desc(item1)
+			.item2(WeatherUtil.getClothesIcon(iconNum2)).item2Desc(item2)
+			.item3(WeatherUtil.getClothesIcon(iconNum3)).item3Desc(item3)
+			.item4(WeatherUtil.getClothesIcon(iconNum4)).item4Desc(item4)
 			.build();
 	}
 }
